3c997df3beb868e35da3351e31c1a3e39390b361
Anton Bernatskiy (kohama) <abernats@uvm.edu>
2024-08-20 16:14:48 +0300

    A working TypeORM-based version
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/apply.ts
    	modified:   src/regenerate.ts

--- src/apply.ts
+++ src/apply.ts
@@ -2,15 +2,15 @@ import fs from "fs"
 import axios from "axios"
 import {program} from "commander"
 import {runProgram} from "@subsquid/util-internal"
-//import {registerTsNodeIfRequired} from '@subsquid/util-internal-ts-node'
+import {registerTsNodeIfRequired} from '@subsquid/util-internal-ts-node'
 import {HASURA_URL, CONFIG_DIR, hasuraHttpHeaders} from "./common"
 
 runProgram(async () => {
     program.description('Apply all configuration calls').parse()
 
-//    await registerTsNodeIfRequired()
+    await registerTsNodeIfRequired()
 
-    const queryData: {filename: string, endpoint: string, query: string}[] = Array()
+    const queryData: ({filename: string, endpoint: string, query: string} | undefined)[] = Array()
     for (let filename of fs.readdirSync(CONFIG_DIR)) {
         const idx = Number(filename.split('-')[0])
         const endpoint = '/'.concat(filename.split('-')[1].split('%2F').join('/'))
@@ -22,6 +22,8 @@ runProgram(async () => {
     }
 
     for (let qdata of queryData) {
+        if (qdata === undefined) continue
+
         process.stdout.write(`Applying ${qdata.filename} ... `)
         const res = await axios.post(
             `${HASURA_URL}${qdata.endpoint}`,
--- src/regenerate.ts
+++ src/regenerate.ts
@@ -1,210 +1,181 @@
+import assert from "assert"
 import axios from "axios"
 import {program} from "commander"
+import {getMetadataArgsStorage} from "typeorm"
+import {MetadataArgsStorage} from "typeorm/metadata-args/MetadataArgsStorage"
+import {RelationTypeInFunction} from "typeorm/metadata/types/RelationTypeInFunction"
 import {runProgram} from "@subsquid/util-internal"
+import {toSnakeCase} from "@subsquid/util-naming"
 import {OutDir} from "@subsquid/util-internal-code-printer"
-import {HASURA_URL, CONFIG_DIR, hasuraHttpHeaders} from "./common"
-//import {registerTsNodeIfRequired} from '@subsquid/util-internal-ts-node'
+import {CONFIG_DIR} from "./common"
+import {registerTsNodeIfRequired, isTsNode} from '@subsquid/util-internal-ts-node'
 
+const OUT_FILE_IDX_INCREMENT = 10
 
 runProgram(async () => {
-    program.description('Analyze the current database state and generate a Hasura configuration that tracks all available tables and foreign key relationships').parse()
+    program.description('Analyze TypeORM models and generate a Hasura configuration that tracks all related tables and foreign key relationships').parse()
 
-//    await registerTsNodeIfRequired()
+    await registerTsNodeIfRequired()
 
-    const tables: TableRecord[] = await getTablesData()
-    const relationships: RelationshipRecord[] = await getRelationshipsData()
+    // Required for getMetadataArgsStorage() to work
+    const modelPath =
+        isTsNode() ?
+        `${process.cwd()}/lib/model/index.ts` :
+        `${process.cwd()}/lib/model/index.js`
+    // @ts-ignore
+    const model = await import(modelPath)
+
+    const typeormMetadata: MetadataArgsStorage = getMetadataArgsStorage()
+
+    const tables: string[] = getTablesData(typeormMetadata)
+    const relationships: RelationshipRecord[] = getRelationshipsData(typeormMetadata)
 
     const dir = new OutDir(CONFIG_DIR)
-    let outIdx = 0
+    let outIdx = OUT_FILE_IDX_INCREMENT
     dir.del()
 
     outIdx = writeTablesConfiguration(tables, dir, outIdx)
     outIdx = writeRelationshipsConfiguration(relationships, dir, outIdx)
 })
 
-type TableRecord = {
-    name: string
-    schema: string
-}
 
 type RelationshipRecord = {
-    id: string
     from: string
     field: string
     to: string
     oneToOne: boolean
 }
 
-async function getTablesData(): Promise<TableRecord[]> {
-    const HASURA_METADATA_API_URL = `${HASURA_URL}/v1/metadata`
 
-    const listTablesQuery = {
-        type: 'pg_get_source_tables',
-        args: {
-            source: 'default'
-        }
-    }
-
-    const res = await axios.post(
-        HASURA_METADATA_API_URL,
-        listTablesQuery,
-        {
-            headers: hasuraHttpHeaders,
-            validateStatus: () => true
-        }
-    )
-
-    if (res.status !== 200) {
-        console.error(`Source table listing via ${HASURA_METADATA_API_URL} failed with HTTP ${res.status} ${res.statusText}`, res.data)
-        process.exit(1)
-    }
-
-    return res.data.filter((t: TableRecord) => t.name !== 'migrations' && t.schema === 'public')
+function getTablesData(metadata: MetadataArgsStorage): string[] {
+    return metadata.tables.map(t => tableName(t.target))
 }
 
-async function getRelationshipsData(): Promise<RelationshipRecord[]> {
-    const HASURA_SCHEMA_API_URL = `${HASURA_URL}/v2/query`
 
-    const listRelationshipsQuery = {
-        type: 'run_sql',
-        args: {
-            source: 'default',
-            sql: `
-                SELECT tc.constraint_name,
-                    tc.table_name,
-                    tc.constraint_type,
-                    kcu.column_name,
-                    ccu.table_name AS foreign_table_name,
-                    ccu.column_name AS foreign_column_name
-                FROM information_schema.table_constraints AS tc
-                JOIN information_schema.key_column_usage AS kcu
-                    ON tc.constraint_name = kcu.constraint_name
-                    AND tc.table_schema = kcu.table_schema
-                JOIN information_schema.constraint_column_usage AS ccu
-                    ON ccu.constraint_name = tc.constraint_name
-                WHERE
-                    (
-                        tc.constraint_type = 'FOREIGN KEY'
-                        OR tc.constraint_type = 'UNIQUE'
-                    )
-                    AND tc.table_schema = 'public';
-            `
-        }
+function tableName(entityTarget: string | Function): string {
+    if (typeof entityTarget === 'string') {
+        return entityTarget
     }
-
-    const res = await axios.post(
-        HASURA_SCHEMA_API_URL,
-        listRelationshipsQuery,
-        {
-            headers: hasuraHttpHeaders,
-            validateStatus: () => true
-        }
-    )
-
-    if (res.status !== 200) {
-        console.error(`Relationship listing via ${HASURA_SCHEMA_API_URL} failed with HTTP ${res.status} ${res.statusText}`, res.data)
-        process.exit(1)
+    else {
+        return toSnakeCase(entityTarget.name)
     }
-
-    if (res.data.result_type !== 'TuplesOk') {
-        console.error(`Unexpected result type received while listing relationships: ${res.data.result_type}`)
-        process.exit(1)
-    }
-
-    const relationships: Map<string, RelationshipRecord> = new Map()
-    for (let fkrec of res.data.result.slice(1)) {
-        let [id, from, type, field, to, toField] = fkrec
-        if (type !== 'FOREIGN KEY') continue
-        if (toField !== 'id') {
-            console.error(`Unusual schema detected: foreign key ${id} is pointing to a non-ID field ${toField} of table ${to}. Refusing to operate`)
-            process.exit(1)
-        }
-        relationships.set(`${fkrec[1]}-${fkrec[3]}`, { id, from, field, to, oneToOne: false })
-    }
-    for (let urec of res.data.result.slice(1)) {
-        if (urec[2] !== 'UNIQUE') continue
-        const rel = relationships.get(`${urec[1]}-${urec[3]}`)
-        if (rel != null) rel.oneToOne = true
-    }
-
-    return [...relationships.values()]
 }
 
-function writeTablesConfiguration(tables: TableRecord[], outDir: OutDir, startingIdx: number): number {
+
+function getRelationshipsData(metadata: MetadataArgsStorage): RelationshipRecord[] {
+    const out: RelationshipRecord[] = []
+    for (let rel of metadata.relations) {
+        // relationships marked as one-to-many are derived and should be ignored here
+        if (rel.relationType === 'many-to-one') {
+            out.push({
+                from: tableName(rel.target),
+                field: `${rel.propertyName}_id`,
+                to: typeToTableName(rel.type),
+                oneToOne: false
+            })
+        }
+        // relationships with defined .inverseSideProperty are derived and should be ignored here
+        if (rel.relationType === 'one-to-one' && rel.inverseSideProperty === undefined) {
+            out.push({
+                from: tableName(rel.target),
+                field: `${rel.propertyName}_id`,
+                to: typeToTableName(rel.type),
+                oneToOne: true
+            })
+        }
+    }
+    return out
+}
+
+
+function typeToTableName(rtype: RelationTypeInFunction): string {
+    try {
+        let callableType: any = rtype
+        let tableName = callableType().name
+        assert(tableName)
+        return toSnakeCase(tableName as string)
+    }
+    catch (e) {
+        console.error(`Non-callable type returned by TypeORM for a relation`, e, rtype)
+        process.exit(1)
+    }
+}
+
+
+function writeTablesConfiguration(tables: string[], outDir: OutDir, startingIdx: number): number {
     for (let table of tables) {
         const trackTableQuery = {
             type: 'pg_track_table',
             args: {
                 source: 'default',
-                table: table.name,
+                table: table,
                 configuration: {}
             }
         }
 
         const outIdxLabel = `${startingIdx}`.padStart(5, '0')
-        const outFileName = `${outIdxLabel}-v1%2Fmetadata-${table.name}.json`
+        const outFileName = `${outIdxLabel}-v1%2Fmetadata-${table}.json`
         outDir.write(outFileName, JSON.stringify(trackTableQuery, null, 2))
-        startingIdx += 1
+        startingIdx += OUT_FILE_IDX_INCREMENT
     }
     return startingIdx
 }
+
 
 function writeRelationshipsConfiguration(constraints: RelationshipRecord[], outDir: OutDir, startingIdx: number): number {
     for (let constraint of constraints) {
         const constraintLabel = `${constraint.from}-${constraint.field}-${constraint.to}`
 
-        const trackForwardRelationshipQuery =
-//            constraint.oneToOne ?
-            {
-				type: 'pg_create_object_relationship',
-				args: {
-					table: constraint.from,
-					name: constraint.to,
-					source: 'default',
-					using: {
-						foreign_key_constraint_on: constraint.field
-					}
-				}
+        const trackForwardRelationshipQuery = {
+            type: 'pg_create_object_relationship',
+            args: {
+                table: constraint.from,
+                name: constraint.to,
+                source: 'default',
+                using: {
+                    foreign_key_constraint_on: constraint.field
+                }
             }
+        }
         let outIdxLabel = `${startingIdx}`.padStart(5, '0')
         let outFileName = `${outIdxLabel}-v1%2Fmetadata-${constraintLabel}-fwd.json`
         outDir.write(outFileName, JSON.stringify(trackForwardRelationshipQuery, null, 2))
-        startingIdx += 1
+        startingIdx += OUT_FILE_IDX_INCREMENT
 
         const trackBackwardRelationshipQuery =
             constraint.oneToOne ?
             {
-				type: 'pg_create_object_relationship',
-				args: {
-					table: constraint.to,
-					name: constraint.from,
-					source: 'default',
-					using: {
-						foreign_key_constraint_on: {
-							table: constraint.from,
-							columns: constraint.field
-						}
-					}
-				}
+                type: 'pg_create_object_relationship',
+                args: {
+                    table: constraint.to,
+                    name: constraint.from,
+                    source: 'default',
+                    using: {
+                        foreign_key_constraint_on: {
+                            table: constraint.from,
+                            columns: constraint.field
+                        }
+                    }
+                }
             } :
             {
-				type: 'pg_create_array_relationship',
-				args: {
-					table: constraint.to,
-					name: constraint.from.concat('s'),
-					source: 'default',
-					using: {
-						foreign_key_constraint_on: {
-							table: constraint.from,
-							columns: constraint.field
-						}
-					}
-				}
+                type: 'pg_create_array_relationship',
+                args: {
+                    table: constraint.to,
+                    name: constraint.from.concat('s'),
+                    source: 'default',
+                    using: {
+                        foreign_key_constraint_on: {
+                            table: constraint.from,
+                            columns: constraint.field
+                        }
+                    }
+                }
             }
         outIdxLabel = `${startingIdx}`.padStart(5, '0')
         outFileName = `${outIdxLabel}-v1%2Fmetadata-${constraintLabel}-bwd.json`
         outDir.write(outFileName, JSON.stringify(trackBackwardRelationshipQuery, null, 2))
-        startingIdx += 1
+        startingIdx += OUT_FILE_IDX_INCREMENT
     }
     return startingIdx
 }
